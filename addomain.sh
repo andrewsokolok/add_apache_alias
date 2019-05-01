37799377f4d4600264c338fd842764b89b28b85c
andrewsokolok <50184330+andrewsokolok@users.noreply.github.com>
2019-05-01 21:20:51 +0000

    Update addomain.sh

--- addomain.sh
+++ addomain.sh
@@ -2,4 +2,5 @@
 echo -n "Type your new domain name: "; read newdomainname
 sed "s/ServerAlias.*/& $newdomainname/" -i /etc/apache2/sites-enabled/*.conf
 certbot --apache --register-unsafely-without-email;
-exit;
+echo -e $"Complete!\nYou just add new alias  $newdomainname to Virtual Host config"
+exit 0;
